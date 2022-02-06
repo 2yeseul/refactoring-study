6eed34b54c593278af73996eda2d577f50cee4b4
이예슬 <20152917@sungshin.ac.kr>
2022-02-06 16:18:25 +0900

    Refactor: Extract Amount Method

---
+++ src/ch01/refactored.ts
@@ -0,0 +1,65 @@
+import { Invoice, Play } from './theater'
+
+export function statement(invoice: Invoice, plays: Play) {
+  let totalAmount = 0
+  let volumeCredits = 0
+
+  let result = `청구 내역 (고객명: ${invoice.customer})\n`
+  const format = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+    minimumFractionDigits: 2,
+  })
+
+  for (const performance of invoice.performances) {
+    const play = plays[performance.playId]
+
+    // 한 번의 공연에 대한 요금 계산
+    // 코드 조각을 별도 함수로 추출하여 분석한 코드에 대한 정보를 반영
+
+    const thisAmount = amountFor(performance, play)
+
+    volumeCredits += Math.max(performance.audience - 30, 0)
+    if (play.type === 'comedy') {
+      volumeCredits += Math.floor(performance.audience / 5)
+    }
+
+    result += ` ${play.name}: ${format.format(thisAmount / 100)} (${
+      performance.audience
+    }석)\n`
+    totalAmount += thisAmount
+  }
+  result += `총액: ${format.format(totalAmount / 100)}\n`
+  result += `적립 포인트: ${volumeCredits}\n`
+
+  return result
+}
+
+// 값이 바뀌지 않는 변수는 매개변수로 전달
+function amountFor(
+  performance: { playId: string; audience: number },
+  play: Play[keyof Play],
+) {
+  let thisAmount = 0
+
+  switch (play.type) {
+    case 'tragedy':
+      thisAmount = 40000
+      if (performance.audience > 30) {
+        thisAmount += 1000 * (performance.audience - 30)
+      }
+      break
+    case 'comedy':
+      thisAmount = 30000
+      if (performance.audience > 20) {
+        thisAmount += 10000 + 500 * (performance.audience - 20)
+      }
+      thisAmount += 300 * performance.audience
+      break
+    default:
+      throw new Error(`알 수 없는 장르: ${play.type}`)
+  }
+
+  // 함수 내에서 바뀌는 변수 반환
+  return thisAmount
+}
