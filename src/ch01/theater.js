a22f7a03a14de0b894e29fa7b33fe5b9c7971672
이예슬 <20152917@sungshin.ac.kr>
2022-02-10 02:03:07 +0900

    Fix: build error

---
+++ src/ch01/theater.js
@@ -0,0 +1,17 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.PLAYS = void 0;
+exports.PLAYS = {
+    hamlet: {
+        name: 'Hamlet',
+        type: 'tragedy',
+    },
+    'as-like': {
+        name: 'As You Like It',
+        type: 'comedy',
+    },
+    othello: {
+        name: 'Othello',
+        type: 'tragedy',
+    },
+};
