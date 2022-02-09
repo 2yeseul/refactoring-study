7c9837231b62478002f6b7e545c980f00a2b4e3e
이예슬 <20152917@sungshin.ac.kr>
2022-02-10 02:00:45 +0900

    Test: fix

--- src/ch01/refactored.test.ts
+++ src/ch01/refactored.test.ts
@@ -1,7 +1,7 @@
 import { statement } from './refactored'
-import * as INVOICES from './invoice.json'
+import INVOICES from './invoice.json'
 
-test('statement test', () => {
+test('[ch01] statement test', () => {
   const expected =
     '청구 내역 (고객명: BigCo)\n Hamlet: $650.00 (55석)\n As You Like It: $580.00 (35석)\n Othello: $500.00 (40석)\n총액: $1,730.00\n적립 포인트: 47점\n'
 
